552e8b5e701899e36439f63614776b09656f68a6
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2021-12-02 20:32:08 +0100

    initial commit with working example

---
+++ example.py
@@ -0,0 +1,11 @@
+import EasyGA
+
+# Create the Genetic algorithm
+ga = EasyGA.GA()
+
+# Evolve the genetic algorithm
+ga.evolve()
+
+# Print your default genetic algorithm
+ga.print_generation()
+ga.print_population()
